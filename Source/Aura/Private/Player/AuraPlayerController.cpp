acd60e8bbda090a5c46496d769d8acbeea18854d
Kaan Kirant <kaankirant98@gmail.com>
2025-08-27 21:37:56 +0300

    Added CursorTrace function and covered all cases.

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -4,10 +4,77 @@
 #include "Player/AuraPlayerController.h"
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+	if (!CursorHit.bBlockingHit) return;
+	
+	LastActor = ThisActor;
+	ThisActor = CursorHit.GetActor();
+
+	/*
+	 * Line trace from cursor. There are several scenairos:
+	 * A. Last Actor is null && This Actor is null.
+	 *		-Do nothing.
+	 *	B.Last Actor is null && This Actor is valid.
+	 *		-Highlight this actor.
+	 *	C.Last Actor is valid && this actor is null.
+	 *		-Unhighlight last actor.
+	 *	D.Last actor is valid && this actor is valid. But last actor != this actor
+	 *		Unhighlight last actor && highlight this actor.
+	 *	E.Last actor is valid && this actor is valid. But last actor == this actor
+	 *		Do nothing.
+	 */
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			//Case B
+			ThisActor->HighlightActor();
+		}
+		else
+		{
+			// Case A - Do nothing.
+		}
+	}
+	else // Last actor is valid
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else // Both actors valid
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+				// Case E - Do nothing
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -52,3 +119,4 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 }
+
