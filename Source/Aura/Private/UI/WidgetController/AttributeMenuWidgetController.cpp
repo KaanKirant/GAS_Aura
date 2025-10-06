fb294db741d8f553f7373650bc456f0ca814b7a3
Kaan Kirant <kaankirant98@gmail.com>
2025-10-07 02:44:38 +0300

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright Kaan Kirant
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
